cc7b4e87b71b33920cd295464c4890ed47c5b2fc
GyVys <Vysniahtc@gmail.com>
2023-12-03 19:36:55 +0000

    adding speed

--- src/minihw_github_gytis/Car.java
+++ src/minihw_github_gytis/Car.java
@@ -14,17 +14,19 @@ public class Car {
     String model;
     int numOfWheels;
     String colour;
+    int speed;
         
-        public Car(String make, String model, int numOfWheels, String colour) {
+        public Car(String make, String model, int numOfWheels, String colour, int speed) {
             
             this.make = make;
             this.model = model;
             this.numOfWheels = numOfWheels;
             this.colour = colour;
+            this.speed = speed;
     
         }
         
         public void drive(String direction) {
-        System.out.println("The " + colour + " " + make + " " + model + " is driving " + direction);
+        System.out.println("The " + colour + " " + make + " " + model + " on " + numOfWheels + " wheels is driving " + direction + " @" + speed + " km/h");
         }
 }
