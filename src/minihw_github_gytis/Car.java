d4a686104a8d537959c5f2ca9b48858932d08f81
GyVys <Vysniahtc@gmail.com>
2023-12-03 19:25:31 +0000

    Initial commit

---
+++ src/minihw_github_gytis/Car.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minihw_github_gytis;
+
+/**
+ *
+ * @author Gytis
+ */
+public class Car {
+    
+    String make;
+    String model;
+    int numOfWheels;
+    String colour;
+        
+        public Car(String make, String model, int numOfWheels, String colour) {
+            
+            this.make = make;
+            this.model = model;
+            this.numOfWheels = numOfWheels;
+            this.colour = colour;
+    
+        }
+        
+        public void drive(String direction) {
+        System.out.println("The " + colour + " " + make + " " + model + " is driving " + direction);
+        }
+}
---
+++ src/minihw_github_gytis/MiniHW_GitHub_Gytis.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihw_github_gytis;
+
+/**
+ *
+ * @author Gytis
+ */
+public class MiniHW_GitHub_Gytis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Car car1 = new Car("Ford", "Focus", 4, "Silver");
+        car1.drive("forward");
+    }
+    
+}
