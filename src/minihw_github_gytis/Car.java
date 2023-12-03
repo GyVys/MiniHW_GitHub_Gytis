2d4a20d67eb374d6304645806b62d4c1c478d733
GyVys <Vysniahtc@gmail.com>
2023-12-03 19:44:24 +0000

    removed number of wheels

--- src/minihw_github_gytis/Car.java
+++ src/minihw_github_gytis/Car.java
@@ -12,21 +12,19 @@ public class Car {
     
     String make;
     String model;
-    int numOfWheels;
     String colour;
     int speed;
         
-        public Car(String make, String model, int numOfWheels, String colour, int speed) {
+        public Car(String make, String model, String colour, int speed) {
             
             this.make = make;
             this.model = model;
-            this.numOfWheels = numOfWheels;
             this.colour = colour;
             this.speed = speed;
     
         }
         
         public void drive(String direction) {
-        System.out.println("The " + colour + " " + make + " " + model + " on " + numOfWheels + " wheels is driving " + direction + " @" + speed + " km/h");
+        System.out.println("The " + colour + " " + make + " " + model + " is driving " + direction + " @" + speed + " km/h");
         }
 }
